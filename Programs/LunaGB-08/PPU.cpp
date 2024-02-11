8d377a312ffe613cc74f025431a2271526e893cb
JX-Master <jxmaster@yeah.net>
2024-02-11 21:11:12 +0800

    Add chapter 08 code.

---
+++ Programs/LunaGB-08/PPU.cpp
@@ -0,0 +1,132 @@
+#include "PPU.hpp"
+#include "Emulator.hpp"
+
+void PPU::increase_ly(Emulator* emu)
+{
+    ++ly;
+    if(ly == lyc)
+    {
+        set_lyc_flag();
+        if(lyc_int_enabled())
+        {
+            emu->int_flags |= INT_LCD_STAT;
+        }
+    }
+    else
+    {
+        reset_lyc_flag();
+    }
+}
+void PPU::init()
+{
+    lcdc = 0x91;
+    lcds = 0;
+    scroll_y = 0;
+    scroll_x = 0;
+    ly = 0;
+    lyc = 0;
+    dma = 0;
+    bgp = 0xFC;
+    obp0 = 0xFF;
+    obp1 = 0xFF;
+    wy = 0;
+    wx = 0;
+    set_mode(PPUMode::oam_scan);
+
+    line_cycles = 0;
+}
+void PPU::tick(Emulator* emu)
+{
+    if(!enabled()) return;
+    ++line_cycles;
+    switch(get_mode())
+    {
+        case PPUMode::oam_scan:
+            tick_oam_scan(emu); break;
+        case PPUMode::drawing:
+            tick_drawing(emu); break;
+        case PPUMode::hblank:
+            tick_hblank(emu); break;
+        case PPUMode::vblank:
+            tick_vblank(emu); break;
+        default: 
+            lupanic(); break;
+    }
+}
+u8 PPU::bus_read(u16 addr)
+{
+    luassert(addr >= 0xFF40 && addr <= 0xFF4B);
+    return ((u8*)(&lcdc))[addr - 0xFF40];
+}
+void PPU::bus_write(u16 addr, u8 data)
+{
+    luassert(addr >= 0xFF40 && addr <= 0xFF4B);
+    if(addr == 0xFF41) // the lower 3 bits are read only.
+    {
+        lcds = (lcds & 0x07) | (data & 0xF8);
+        return;
+    }
+    if(addr == 0xFF44) return; // read only.
+    ((u8*)(&lcdc))[addr - 0xFF40] = data;
+}
+void PPU::tick_oam_scan(Emulator* emu)
+{
+    if(line_cycles >= 80)
+    {
+        set_mode(PPUMode::drawing);
+    }
+}
+void PPU::tick_drawing(Emulator* emu)
+{
+    if(line_cycles >= 369)
+    {
+        set_mode(PPUMode::hblank);
+        if(hblank_int_enabled())
+        {
+            emu->int_flags |= INT_LCD_STAT;
+        }
+    }
+}
+void PPU::tick_hblank(Emulator* emu)
+{
+    if(line_cycles >= PPU_CYCLES_PER_LINE)
+    {
+        increase_ly(emu);
+        if(ly >= PPU_YRES)
+        {
+            set_mode(PPUMode::vblank);
+            emu->int_flags |= INT_VBLANK;
+            if(vblank_int_enabled())
+            {
+                emu->int_flags |= INT_LCD_STAT;
+            }
+        }
+        else
+        {
+            set_mode(PPUMode::oam_scan);
+            if(oam_int_enabled())
+            {
+                emu->int_flags |= INT_LCD_STAT;
+            }
+        }
+        line_cycles = 0;
+    }
+}
+void PPU::tick_vblank(Emulator* emu)
+{
+    if(line_cycles >= PPU_CYCLES_PER_LINE)
+    {
+        increase_ly(emu);
+        if(ly >= PPU_LINES_PER_FRAME)
+        {
+            // move to next frame.
+            set_mode(PPUMode::oam_scan);
+            ly = 0;
+            if(oam_int_enabled())
+            {
+                emu->int_flags |= INT_LCD_STAT;
+            }
+        }
+        line_cycles = 0;
+    }
+}
---
+++ Programs/LunaGB-08/PPU.hpp
@@ -0,0 +1,76 @@
+#pragma once
+#include <Luna/Runtime/MemoryUtils.hpp>
+
+using namespace Luna;
+
+enum class PPUMode : u8
+{
+    hblank = 0,
+    vblank = 1,
+    oam_scan = 2,
+    drawing = 3
+};
+constexpr u32 PPU_LINES_PER_FRAME = 154;
+constexpr u32 PPU_CYCLES_PER_LINE = 456;
+constexpr u32 PPU_YRES = 144;
+constexpr u32 PPU_XRES = 160;
+struct Emulator;
+struct PPU
+{
+    //! 0xFF40 - LCD control.
+    u8 lcdc;
+    //! 0xFF41 - LCD status.
+    u8 lcds;
+    //! 0xFF42 - SCY.
+    u8 scroll_y;
+    //! 0xFF43 - SCX.
+    u8 scroll_x;
+    //! 0xFF44 - LY LCD Y coordinate [Read Only].
+    u8 ly;
+    //! 0xFF45 - LYC LCD Y Compare.
+    u8 lyc;
+    //! 0xFF46 - DMA value.
+    u8 dma;
+    //! 0xFF47 - BGP (BG palette data).
+    u8 bgp;
+    //! 0xFF48 - OBP0 (OBJ0 palette data).
+    u8 obp0;
+    //! 0xFF49 - OBP1 (OBJ1 palette data).
+    u8 obp1;
+    //! 0xFF4A - WY (Window Y position plus 7).
+    u8 wy;
+    //! 0xFF4B - WX (Window X position plus 7).
+    u8 wx;
+
+    // PPU internal state.
+
+    //! The number of cycles used for this scan line.
+    u32 line_cycles;
+
+    bool enabled() const { return bit_test(&lcdc, 7); }
+
+    PPUMode get_mode() const { return (PPUMode)(lcds & 0x03); }
+    void set_mode(PPUMode mode)
+    {
+        lcds &= 0xFC; // clears previous mode.
+        lcds |= (u8)(mode);
+    }
+    void set_lyc_flag() { bit_set(&lcds, 2); }
+    void reset_lyc_flag() { bit_reset(&lcds, 2); }
+    bool hblank_int_enabled() const { return !!(lcds & (1 << 3)); }
+    bool vblank_int_enabled() const { return !!(lcds & (1 << 4)); }
+    bool oam_int_enabled() const { return !!(lcds & (1 << 5)); }
+    bool lyc_int_enabled() const { return !!(lcds & (1 << 6)); }
+
+    void increase_ly(Emulator* emu);
+
+    void init();
+    void tick(Emulator* emu);
+    u8 bus_read(u16 addr);
+    void bus_write(u16 addr, u8 data);
+
+    void tick_oam_scan(Emulator* emu);
+    void tick_drawing(Emulator* emu);
+    void tick_hblank(Emulator* emu);
+    void tick_vblank(Emulator* emu);
+};
